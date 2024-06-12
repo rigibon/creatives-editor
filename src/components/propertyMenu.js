cc5e1a2eb7cf7a9281944e63722c409b2d75de63
rigibon <kenugba@gmail.com>
2024-06-12 12:28:40 -0300

    Deploy URL

--- src/components/propertyMenu.js
+++ src/components/propertyMenu.js
@@ -40,7 +40,7 @@ const PropertyMenu = ({ props, handleChange, images, colors, onChangeSize }) => 
 		setColorModalVisible(false);
 	};
 
-	const apiUrl = "http://localhost:5500";
+	const apiUrl = process.env.NODE_ENV === "production" ? process.env.REACT_APP_API_PROD : process.env.REACT_APP_API_DEV;
 
 	const handleTabChange = async (e) => {
 		const requestData = {
--- src/screens/editor.js
+++ src/screens/editor.js
@@ -47,7 +47,7 @@ const Editor = () => {
 
 	const [messageApi, contextHolder] = message.useMessage();
 
-	const apiUrl = "http://localhost:5500";
+	const apiUrl = process.env.NODE_ENV === "production" ? process.env.REACT_APP_API_PROD : process.env.REACT_APP_API_DEV;
 
 	const location = useLocation();
 	const queryParams = new URLSearchParams(location.search);
